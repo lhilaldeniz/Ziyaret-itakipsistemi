e46d220c5c3f81592fcfbd48d3cafcd28a4d2c65
lhilaldeniz <hilaldeniz941@gmail.com>
2025-05-22 21:35:06 +0300

    Update app.py

--- ziyaretçi dosyası yeni/ziyaretçi dosyası yeni/app.py
+++ ziyaretçi dosyası yeni/ziyaretçi dosyası yeni/app.py
@@ -249,5 +249,10 @@ def randevu_liste():
 
 #if __name__ == "__main__":
  #   with app.app_context():
-  #      db.create_all()
-  #  app.run(debug=True)
+ #      db.create_all()
+ #  app.run(debug=True)
+
+
+import os
+if _name_== "_main_":
+    app.run(host="0.0.0.0 , port=int(os.environ.get("PORT , 5000)))
